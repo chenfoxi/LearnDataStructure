ca19f01dc0fc85f79100bc1d1db0541ce11091ae
foxi.chen <ianchen0403@gmail.com>
2021-03-24 17:46:29 +1300

    remove the duplicates of the array (in-place)
    change author name

--- ArrayExe/src/main/java/chen/nz/InPlaceArrayReplacement.java
+++ ArrayExe/src/main/java/chen/nz/InPlaceArrayReplacement.java
@@ -27,4 +27,29 @@ public class InPlaceArrayReplacement {
         }
         return arr;
     }
+
+    /**
+     * Given a sorted array nums, remove the duplicates in-place
+     * such that each element appears only once and returns the new length.
+     * Do not allocate extra space for another array, you must do this
+     * by modifying the input array in-place with O(1) extra memory.
+     * Input: nums = [1,1,2]
+     * Output: 2, nums = [1,2]
+     * @param nums input nums
+     * @return the length of unique elements in the array
+     */
+    public int removeDuplicates(int[] nums) {
+        // two pointers
+        int length = nums.length;
+        int i = 0, j = i + 1;
+        while( j < length) {
+            if (nums[j] != nums[i]) {
+                nums[i + 1] = nums[j];
+                i++;
+            }
+            j++;
+        }
+        length = i + 1;
+        return length;
+    }
 }
