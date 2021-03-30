3feef3b8f934c2ba1c150ced38c1beb61a354026
foxi.chen <ianchen0403@gmail.com>
2021-03-31 11:42:18 +1300

    add sortarrayparity

--- ArrayExe/src/main/java/chen/nz/InPlaceArrayReplacement.java
+++ ArrayExe/src/main/java/chen/nz/InPlaceArrayReplacement.java
@@ -78,4 +78,27 @@ public class InPlaceArrayReplacement {
            }
        }
     }
+
+    /**
+     * Given an array A of non-negative integers,
+     * return an array consisting of all the even elements of A,
+     * followed by all the odd elements of A.
+     * Input: [3,1,2,4]
+     * Output: [2,4,3,1]
+     * The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+     * @param A
+     * @return
+     */
+    public int[] sortArrayByParity(int[] A) {
+        int odd = 0, len = A.length;
+        for(int even = 0; even < len; even++) {
+            if(A[even] % 2 == 0) {
+                int temp = A[odd];
+                A[odd] = A[even];
+                A[even] = temp;
+                odd++;
+            }
+        }
+        return A;
+    }
 }
